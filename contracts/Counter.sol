117758ede8131c75299d371f322cf66b97565aa0
lixian7531-sudo <lixian7531@gmail.com>
2026-09-07 17:41:27 +0800

    firstcontract

---
+++ contracts/Counter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint public counter;
+
+    function count() public {
+        counter = counter + 1;
+    }
+
+    function add(uint x) public {
+        counter = counter + x;
+    }
+    
+}
